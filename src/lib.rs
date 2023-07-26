9008c921972ea774695d93e9d96279ae9b1f3fd1
anand jain <anandj@uchicago.edu>
2023-07-26 15:24:26 -0400

    made list of ids

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,37 @@
 use leptos::{error::Result, *};
-use thiserror::Error;
 use serde_json::Value;
+use thiserror::Error;
+use std::collections::HashMap;
+use serde_derive::{Serialize, Deserialize};
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct OverpassResponse {
+    pub elements: Vec<Element>,
+    pub generator: String,
+    pub osm3s: Osm3s,
+    pub version: f64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Osm3s {
+    pub copyright: String,
+    #[serde(rename = "timestamp_osm_base")]
+    pub timestamp_osm_base: String,
+}
+
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Element {
+    pub id: i64,
+    pub lat: f64,
+    pub lon: f64,
+    pub tags: HashMap<String, String>,
+    #[serde(rename = "type")]
+    pub type_field: String,
+}
 
 #[derive(Error, Clone, Debug)]
 pub enum BathroomError {
@@ -8,7 +39,7 @@ pub enum BathroomError {
     FetchBathroomsFailed,
 }
 
-async fn fetch_bathrooms(_: ()) -> Result<Value> {
+async fn fetch_bathrooms(_: ()) -> Result<OverpassResponse> {
     let lat = 42.3593101;
     let lon = -71.105846;
 
@@ -19,7 +50,7 @@ async fn fetch_bathrooms(_: ()) -> Result<Value> {
     .send()
     .await?
     // convert it to JSON
-    .json::<Value>()
+    .json::<OverpassResponse>()
     .await?;
     Ok(res)
 }
@@ -48,7 +79,19 @@ pub fn fetch_example(cx: Scope) -> impl IntoView {
     let bathrooms_view = move || {
         bathrooms.read(cx).map(|data| {
             data.map(|data| {
-                view! { cx, <pre>{serde_json::to_string_pretty(&data).unwrap()}</pre> }
+                data.elements.iter().map(|e| {
+                    view! { cx, <h1>{e.id.to_string()}</h1> }
+                }).collect_view(cx)
+                // let els = &data.clone()["elements"];
+                // els.as_array()
+                //     .iter()
+                //     .map(|e| view! { cx, <h1> {e} </h1> })
+                //     .collect_view(cx)
+                // for el in els.as_array().unwrap() {
+                //     view! { cx, <div>{el["tags"]["name"].as_str().unwrap()}</div> };
+                // }
+                // view! { cx, <pre>{serde_json::to_string_pretty(&data).unwrap()}</pre> }
+                // view! { cx, <h1>{"hi"}</h1> }
             })
         })
     };
